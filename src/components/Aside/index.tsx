a627f32080dcb5b54125c88b8d3b86735870789f
Mateus Giroletti <mateusgiroletti97@gmail.com>
2023-04-01 15:22:38 -0300

    styles for aside

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -1,3 +1,4 @@
+import { CheckCircle, XCircle } from "@phosphor-icons/react";
 import "./styles.css";
 
 function Aside() {
@@ -24,8 +25,43 @@ function Aside() {
             <div className="history-result">
                 <ul>
                     <li>
-                        <div>
-                            
+                        <div className="correct-choice">
+                            <div className="correct-color" style={{ background: "#77D353" }}>
+                                <span>#77D353</span>
+                            </div>
+
+                            <div className="score-time">
+                                <CheckCircle size={32} weight="duotone" color="#96c482" />
+                                <span>2s</span>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="wrong-choice">
+                            <div className="choice-color" style={{ background: "#537ED3", marginRight: "1rem" }}>
+                                <span>#537ED3</span>
+                            </div>
+
+                            <div className="choice-color" style={{ background: "#071F50" }}>
+                                <span>#071F50</span>
+                            </div>
+
+                            <div className="score-time">
+                                <XCircle size={32} weight="duotone" color="#ec9090" />
+                                <span>2s</span>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="correct-choice">
+                            <div className="correct-color" style={{ background: "#77D353" }}>
+                                <span>#77D353</span>
+                            </div>
+
+                            <div className="score-time">
+                                <CheckCircle size={32} weight="duotone" color="#96c482" />
+                                <span>2s</span>
+                            </div>
                         </div>
                     </li>
                 </ul>
