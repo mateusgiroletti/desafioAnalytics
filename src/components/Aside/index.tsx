8a047b75b778d6073a5ab1ebf4b010345c4d8051
Mateus Giroletti <mateusgiroletti97@gmail.com>
2023-03-31 21:59:53 -0300

    adding style

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -3,7 +3,33 @@ import "./styles.css";
 function Aside() {
     return (
         <aside>
-            Aside
+            <div className="aside-header">
+                <h1>Current/Latest Game</h1>
+            </div>
+
+            <div className="history-information">
+                <div>
+                    <span>Guessed Color</span>
+                </div>
+
+                <div>
+                    <span>Correct color</span>
+                </div>
+
+                <div>
+                    <span>Score</span>
+                </div>
+            </div>
+
+            <div className="history-result">
+                <ul>
+                    <li>
+                        <div>
+                            
+                        </div>
+                    </li>
+                </ul>
+            </div>
         </aside>
     );
 }
